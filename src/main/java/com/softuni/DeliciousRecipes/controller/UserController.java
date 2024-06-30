54a2a76ec9665f984c84fac5d946b674b7e5fdf2
petincka <petincka@abv.bg>
2024-06-30 17:36:41 +0300

    Security

--- src/main/java/com/softuni/DeliciousRecipes/controller/UserController.java
+++ src/main/java/com/softuni/DeliciousRecipes/controller/UserController.java
@@ -1,22 +1,18 @@
 package com.softuni.DeliciousRecipes.controller;
 
 import com.softuni.DeliciousRecipes.model.dto.UserRegisterDTO;
-import com.softuni.DeliciousRecipes.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @ModelAttribute
     public UserRegisterDTO userRegisterDTO(){
@@ -35,8 +31,5 @@ public class UserController {
     }
 
 
-    @PostMapping("/register")
-    public String doRegister(@Valid UserRegisterDTO userRegisterDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
-        return "redirect:/login";
-    }
+
 }
--- src/main/java/com/softuni/DeliciousRecipes/repo/UserRepository.java
+++ src/main/java/com/softuni/DeliciousRecipes/repo/UserRepository.java
@@ -1,7 +1,13 @@
 package com.softuni.DeliciousRecipes.repo;
 
+import com.softuni.DeliciousRecipes.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository{
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
+
