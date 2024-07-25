75b8255ac0aad126da71d69db3fed217c7e220b4
petincka <petincka@abv.bg>
2024-07-25 20:14:11 +0300

    Add a comment section

--- src/main/java/com/softuni/DeliciousRecipes/web/CommentController.java
+++ src/main/java/com/softuni/DeliciousRecipes/web/CommentController.java
@@ -1,13 +1,28 @@
 package com.softuni.DeliciousRecipes.web;
 
+import com.softuni.DeliciousRecipes.repository.CommentRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CommentController {
+    private final CommentRepository commentRepository;
+
+    public CommentController(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
 
     @GetMapping("/add-comment")
     public String addComment(){
         return "add-comment";
+    }
+
+    @GetMapping("/comments")
+    public String allComments(Model model){
+        if (commentRepository.count() < 0){
+            model.addAttribute("isEmpty", true);
+        }
+        return "comments";
     }
 }
