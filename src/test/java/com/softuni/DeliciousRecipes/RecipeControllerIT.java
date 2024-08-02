6e3291e94b5d5cf26a15a260b2a4987904527cbe
petincka <petincka@abv.bg>
2024-08-02 10:28:33 +0300

    Integration tests

---
+++ src/test/java/com/softuni/DeliciousRecipes/RecipeControllerIT.java
@@ -0,0 +1,25 @@
+package com.softuni.DeliciousRecipes;
+
+import com.softuni.DeliciousRecipes.model.entity.Recipe;
+import com.softuni.DeliciousRecipes.repository.RecipeRepository;
+import com.softuni.DeliciousRecipes.service.RecipeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RecipeControllerIT {
+    @Autowired
+    private RecipeService recipeService;
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @Test
+    public  void testViewRecipeById(){
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipe.setName("Brownie");
+        recipe.setTimeForCooking(10);
+        recipeRepository.save(recipe);
+    }
+}
