ee6c9ab515ac185d070c13bfcff96b9cc94ad8c7
petincka <petincka@abv.bg>
2024-08-02 15:02:05 +0300

    Integration tests

--- src/test/java/com/softuni/DeliciousRecipes/web/RegistrationControllerIT.java
+++ src/test/java/com/softuni/DeliciousRecipes/web/RegistrationControllerIT.java
@@ -1,11 +1,29 @@
 package com.softuni.DeliciousRecipes.web;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RegistrationControllerIT {
 
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void testRegistration() throws Exception {
+        mockMvc.perform(post("/users/register")
+                .param("email", "petincka@example.com")
+                .param("username", "petincka")
+                .param("password", "topSecret")
+        ).andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
 }
